7ed33f1edd332304753b1a56d4edb3eb99161cce
Bird Egop <sampletext32@bk.ru>
2021-03-02 22:28:56 +0300

    Add project

---
+++ Shustrov_4_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Shustrov_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("x=");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.Write("a=");
+            double a = double.Parse(Console.ReadLine());
+
+            double W = 1;
+            int data = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                W += Math.Pow(Math.Cos(x), data) / (Math.Sqrt(Math.Pow(a,data) + Math.Sin(a + i % 2 * Math.PI / 2)));
+                data += 2;
+            }
+            Console.WriteLine("Result="+W);
+        }
+    }
+}
